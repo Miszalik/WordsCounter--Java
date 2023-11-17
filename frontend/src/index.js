f9f7d38c97b31b0207ea659a545bfda7d98bb75f
Mateusz Michalik <mateusz.miszalik@wp.pl>
2023-11-17 17:17:36 +0100

    feat: Ready basic frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import "./index.css";
 import App from "./App";
-//import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
