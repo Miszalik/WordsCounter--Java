652653d2d5831c692d36b9c50e3feefaf63a2d11
Mateusz Michalik <mateusz.miszalik@wp.pl>
2023-11-17 16:05:14 +0100

    feat: Prepared register page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "./store/store";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+//import * as serviceWorker from "./serviceWorker";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
