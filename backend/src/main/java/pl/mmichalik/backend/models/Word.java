94beb16bec8097c8beb58c701bb2ace1edcf6849
Mateusz Michalik <mateusz.miszalik@wp.pl>
2023-11-19 04:18:39 +0100

    chore: Prepared word model

---
+++ backend/src/main/java/pl/mmichalik/backend/models/Word.java
@@ -0,0 +1,37 @@
+package pl.mmichalik.backend.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "words")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long wordId;
+
+    @NotBlank
+    @Size(max = 30)
+    private String wordName;
+
+    @NotBlank
+    private Date creationDate;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "word_to_user", joinColumns = @JoinColumn(name = "word_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> users = new HashSet<>();
+
+}
