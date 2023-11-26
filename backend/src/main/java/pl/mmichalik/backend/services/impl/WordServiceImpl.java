ce66d566b463cda5e9c7da0893e23d066d6ea133
Mateusz Michalik <mateusz.miszalik@wp.pl>
2023-11-26 23:35:04 +0100

    chore: Prepared WordServiceImpl

--- backend/src/main/java/pl/mmichalik/backend/services/impl/WordServiceImpl.java
+++ backend/src/main/java/pl/mmichalik/backend/services/impl/WordServiceImpl.java
@@ -8,6 +8,7 @@ import pl.mmichalik.backend.repository.WordRepository;
 import pl.mmichalik.backend.services.WordService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -22,5 +23,35 @@ public class WordServiceImpl implements WordService {
     @Override
     public List<Word> findAll() {
         return wordRepository.findAll();
+    }
+
+    @Override
+    public Optional<Word> findById(Long id) {
+        return wordRepository.findById(id);
+    }
+
+    @Override
+    public Word save(Word word) {
+        return wordRepository.save(word);
+    }
+
+    @Override
+    public boolean update(Long id, Word word) {
+        Optional<Word> existingWord = wordRepository.findById(id);
+        if (existingWord.isEmpty())
+            return false;
+
+        wordRepository.save(word);
+        return true;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        Optional<Word> existingWord = wordRepository.findById(id);
+        if (existingWord.isEmpty())
+            return false;
+
+        wordRepository.deleteById(id);
+        return true;
     }
 }
